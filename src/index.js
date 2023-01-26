682a70aa2f1aa150d547f52d86bcaf71d3a328e4
dsk90it <dsk90it@gmail.com>
2023-01-26 12:59:50 +0530

    refactored folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/default.css'
+import './styles/defaults.css'
 import Discover from './pages/Discover'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
