45177ed0d9637b5d2bcd17454823b276407a4a11
dsk90it <dsk90it@gmail.com>
2023-01-26 12:56:55 +0530

    renamed home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/default.css'
-import Dashboard from './pages/Dashboard'
+import Discover from './pages/Discover'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <Discover />
   </React.StrictMode>
 )
